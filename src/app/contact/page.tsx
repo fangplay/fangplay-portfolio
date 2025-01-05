a5ac35aabdd709a9cf0e836f9b869d13382b9d8e
fangplay <fangplay_2014@hotmail.com>
2025-01-05 18:04:07 +0700

    error page fix update

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+
+import {
+  Heading,
+  Text,
+  Flex,
+  Button,
+  Grid,
+  Icon,
+  InlineCode,
+  RevealFx,
+  LetterFx,
+  GlitchFx,
+  Arrow,
+  SmartImage,
+  Badge,
+} from "@/once-ui/components";
+import Link from "next/link";
+
+export default function Contact() {
+    
+}
